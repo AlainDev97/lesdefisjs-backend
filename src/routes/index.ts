0de00dba44d4ae995c8b298c0ad8206ee16d5149
Alain Bonneau <alaindev97@gmail.com>
2026-04-20 05:44:15 +0200

    feat/ update router index

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import categoriesRoutes from "../modules/categories/categories.route";
+import challengesRoutes from "../modules/challenges/challenges.route";
 
 const router = Router();
 
 router.use("/categories", categoriesRoutes);
+router.use("/challenges", challengesRoutes);
 
 export default router;
