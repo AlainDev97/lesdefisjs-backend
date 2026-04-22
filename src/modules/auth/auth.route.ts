4f3c9f427b7db3b87bcac04e0e7ec069bb9b1d37
Alain Bonneau <alaindev97@gmail.com>
2026-04-22 14:40:04 +0200

    feat/ add auth router

---
+++ src/modules/auth/auth.route.ts
@@ -0,0 +1,15 @@
+import { Router } from "express";
+import {
+  loginController,
+  meController,
+  registerController,
+} from "./auth.controller";
+import { authMiddleware } from "../../middlewares/auth.middleware";
+
+const authRouter = Router();
+
+authRouter.post("/register", registerController);
+authRouter.post("/login", loginController);
+authRouter.get("/me", authMiddleware, meController);
+
+export default authRouter;
