8d3c085851882c8fbe72c8a4eaf14e28f1aaa7a7
Alain Bonneau <alaindev97@gmail.com>
2026-04-22 14:41:08 +0200

    feat/ add admin middleware

---
+++ src/middlewares/admin.middleware.ts
@@ -0,0 +1,23 @@
+import { NextFunction, Response } from "express";
+import { UserRole } from "../generated/prisma/client";
+import { AuthenticatedRequest } from "./auth.middleware";
+
+export function adminMiddleware(
+  req: AuthenticatedRequest,
+  res: Response,
+  next: NextFunction,
+) {
+  if (!req.user) {
+    return res.status(401).json({
+      message: "Non authentifié",
+    });
+  }
+
+  if (req.user.role !== UserRole.ADMIN) {
+    return res.status(403).json({
+      message: "Accès refusé",
+    });
+  }
+
+  next();
+}
