16ffeea61a13d84466bb8f39a090be51374bb872
Alain Bonneau <alaindev97@gmail.com>
2026-04-23 15:09:25 +0200

    feat/ add auth middleware to submissions router

--- src/modules/submissions/submissions.route.ts
+++ src/modules/submissions/submissions.route.ts
@@ -12,9 +12,14 @@ const submissionRouter = Router();
 submissionRouter.post("/", authMiddleware, createSubmissionController);
 submissionRouter.get(
   "/challenge/:challengeId",
+  authMiddleware,
   getSubmissionsByChallengeController,
 );
-submissionRouter.get("/user/:userId", getSubmissionsByUserController);
-submissionRouter.get("/:id", getSubmissionByIdController);
+submissionRouter.get(
+  "/user/:userId",
+  authMiddleware,
+  getSubmissionsByUserController,
+);
+submissionRouter.get("/:id", authMiddleware, getSubmissionByIdController);
 
 export default submissionRouter;
