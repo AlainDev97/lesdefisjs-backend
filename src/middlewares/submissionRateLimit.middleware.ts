97b0943aa52aa32769209ebb86676c95779a05a9
Alain Bonneau <alaindev97@gmail.com>
2026-06-08 06:11:16 +0200

    feat: add standart header and legacy header in submission rate limit middleware

--- src/middlewares/submissionRateLimit.middleware.ts
+++ src/middlewares/submissionRateLimit.middleware.ts
@@ -3,6 +3,10 @@ import rateLimit from "express-rate-limit";
 export const submissionRateLimit = rateLimit({
   windowMs: 60 * 1000,
   max: 10,
+
+  standardHeaders: true,
+  legacyHeaders: false,
+
   message: {
     message: "Trop de soumissions. Réessaie dans quelques instants.",
   },
