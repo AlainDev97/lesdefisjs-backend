68c3fe3d88ee10adbbf75e42ad4b17f80283a7f1
Alain Bonneau <alaindev97@gmail.com>
2026-06-09 08:49:18 +0200

    feat: add regex to register password

--- src/modules/auth/auth.schema.ts
+++ src/modules/auth/auth.schema.ts
@@ -12,7 +12,14 @@ export const registerSchema = z.object({
   password: z
     .string()
     .min(8, "Le mot de passe doit contenir au moins 8 caractères")
-    .max(100, "Le mot de passe est trop long"),
+    .max(100, "Le mot de passe est trop long")
+    .regex(/[A-Z]/, "Le mot de passe doit contenir au moins une majuscule")
+    .regex(/[a-z]/, "Le mot de passe doit contenir au moins une minuscule")
+    .regex(/[0-9]/, "Le mot de passe doit contenir au moins un chiffre")
+    .regex(
+      /[^A-Za-z0-9]/,
+      "Le mot de passe doit contenir au moins un caractère spécial",
+    ),
 });
 
 export const loginSchema = z.object({
